0538f4d187655d48d925cc870b0c60492d385285
Yassine <yassinemoussafir31@gmail.com>
2024-10-03 11:31:07 +0000

    another test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
+"""Module defining isWinner function."""
 
 
 def isWinner(x, nums):
+    """Function to get who has won in prime game"""
     if not nums or x < 1:
         return None
 
-    def sieve(n):
-        is_prime = [True] * (n + 1)
-        is_prime[0], is_prime[1] = False, False
-        for i in range(2, int(n ** 0.5) + 1):
-            if is_prime[i]:
-                for j in range(i * i, n + 1, i):
-                    is_prime[j] = False
-        return [i for i in range(n + 1) if is_prime[i]]
+    max_num = max(nums)
+    primes = sieve(max_num)
 
-    def count_primes(n):
-        return len(sieve(n))
+    mariaWinsCount = 0
+    benWinsCount = 0
 
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n == 1:
-            ben_wins += 1
-            continue
-
-        prime_count = count_primes(n)
+    for num in nums:
+        prime_count = sum(primes[1:num + 1])
         if prime_count % 2 == 0:
-            ben_wins += 1
+            benWinsCount += 1
         else:
-            maria_wins += 1
+            mariaWinsCount += 1
 
-    if maria_wins > ben_wins:
+    if mariaWinsCount > benWinsCount:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif benWinsCount > mariaWinsCount:
         return "Ben"
     else:
         return None
+
+
+def sieve(n):
+    """Sieve of Eratosthenes to find all prime numbers up to n"""
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
